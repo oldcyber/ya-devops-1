76ff5a6acee2123da40f84e3524c2c57225f8b4e
Anton Tyagniy <oldcyber@gmail.com>
2022-07-13 01:19:36 +0300

    Инкремент 2, попытка 6
    Инкремент 2, попытка 5
    
    Инкремент 2, попытка 4
    
    Инкремент 2, попытка 3
    
    golangci-lint здесь не видит ошибок.
    В репозитории lint есть сообщение о том, что неправильно указывает на ошибку. Время тратить совершенно неохота на это
    
    Странные ошибки на 'Body.Close()' Он есть, а тест ругается.
    
    Немножко психанул и всё переписал. Добавил Makefile
    
    Инкремент1, попытка 1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"ya-devops-1/internal/server"
+)
+
+func main() {
+	server.StoredData = make(map[string]server.StoredType)
+	// http.HandleFunc("/", server.GetRoot)
+	http.HandleFunc("/update/", server.GetMetrics)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ internal/server/handlers.go
@@ -0,0 +1,39 @@
+package server
+
+import (
+	"log"
+	"net/http"
+
+	"ya-devops-1/internal/tools"
+)
+
+// GetRoot - обработчик запроса на главную страницу
+//func GetRoot(w http.ResponseWriter, _ *http.Request) {
+//	w.Header().Set("Content-Type", "text/plain")
+//
+//	_, err := w.Write([]byte("Hello, stranger!"))
+//	if err != nil {
+//		return
+//	}
+//}
+
+// GetMetrics читаем данные из URL и сохраняем
+func GetMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	res := tools.GetURL(r.URL.Path)
+	if res == nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	er, an := storeData(res)
+	if !er {
+		w.WriteHeader(an)
+		return
+	} else {
+		w.WriteHeader(200)
+	}
+
+	for k, v := range StoredData {
+		log.Println("key", k, "value", v)
+	}
+}
