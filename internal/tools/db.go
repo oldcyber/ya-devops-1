24b4f33070558426ccb7be7c4712fd975d2562ec
Anton Tyagniy <oldcyber@gmail.com>
2022-08-23 20:08:33 +0300

    Инкремент 10 попытка 3

--- internal/tools/db.go
+++ internal/tools/db.go
@@ -3,22 +3,15 @@ package tools
 import (
 	"context"
 	"database/sql"
-	"time"
 
 	_ "github.com/lib/pq"
 	log "github.com/sirupsen/logrus"
 )
 
-//type configs interface {
-//	GetDatabaseDSN() string
-//}
-
-// var db *sql.DB
-
 func DBConnect(conn string, ctx context.Context) (*sql.DB, error) {
-	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
-	defer cancel()
-	conn = conn + "&connect_timeout=10"
+	// ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	// defer cancel()
+	conn += "&connect_timeout=10"
 	log.Info("Connecting to database:", conn)
 	db, err := sql.Open("postgres", conn)
 	if err != nil {
@@ -28,21 +21,3 @@ func DBConnect(conn string, ctx context.Context) (*sql.DB, error) {
 	// defer dbPool.Close()
 	return db, nil
 }
-
-// db ping
-//func Ping(ctx context.Context) error {
-//	var err error
-//	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
-//	defer cancel()
-//
-//	_, err = db.ExecContext(ctx, "SELECT pg_sleep(10)")
-//	log.Error(err)
-//	return err
-
-//_, err = db.Exec("SELECT 1")
-//if err != nil {
-//	log.Error(err)
-//	return err
-//}
-//return nil
-//}
